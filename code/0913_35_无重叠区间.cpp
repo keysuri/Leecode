c677b5ba936634ed6e7a05396edd325e4e47f9e2
chenzkun <2053795332@qq.com>
2024-09-14 09:01:05 +0800

    24/09/13贪心

---
+++ code/0913_35_无重叠区间.cpp
@@ -0,0 +1,68 @@
+//给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。返回 需要移除区间的最小数量，使剩余区间互不重
+//叠 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: intervals = [[1,2],[2,3],[3,4],[1,3]]
+//输出: 1
+//解释: 移除 [1,3] 后，剩下的区间没有重叠。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: intervals = [ [1,2], [1,2], [1,2] ]
+//输出: 2
+//解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: intervals = [ [1,2], [2,3] ]
+//输出: 0
+//解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= intervals.length <= 10⁵ 
+// intervals[i].length == 2 
+// -5 * 10⁴ <= starti < endi <= 5 * 10⁴ 
+// 
+//
+// Related Topics 贪心 数组 动态规划 排序 👍 1152 👎 0
+
+#include <bits/stdc++.h>
+using namespace std;
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    static bool cmp(const vector<int>& a, const vector<int>& b) {
+        if (a[0] == b[0]) return a[1] < b[1];
+        else return a[0] < b[0];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), cmp);
+        int result = 0;
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.size(); i ++) {
+            if (intervals[i][0] >= end) {
+                end = intervals[i][1];
+            }
+            else {
+                end = min(end, intervals[i][1]);
+                result ++;
+            }
+        }
+        return result;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+// 也可以右区间排序
