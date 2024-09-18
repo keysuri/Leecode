d686a4349a2e7f62b8ce7840867df1aa8e917755
chenzkun <2053795332@qq.com>
2024-09-18 21:17:37 +0800

    24/09/18动态规划

---
+++ leetcode/editor/cn/240918_[96]不同的二叉搜索树.cpp
@@ -0,0 +1,46 @@
+//给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：n = 3
+//输出：5
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 19 
+// 
+//
+// Related Topics 树 二叉搜索树 数学 动态规划 二叉树 👍 2547 👎 0
+
+#include <bits/stdc++.h>
+using namespace std;
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        for (int a : dp) cout << a << " ";
+        return dp[n];
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
